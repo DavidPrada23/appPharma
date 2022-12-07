826705c38618138532c474235cd5e08c4f64b614
davidprada23 <davidprada23@gmail.com>
2022-12-07 14:36:11 -0500

    subiendo el proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  { 
+    path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) 
+  }, 
+  { 
+    path: 'character-list', loadChildren: () => import('./components/pages/characters/character-list/character-list.module').then(m => m.CharacterListModule) 
+  }, 
+  { 
+    path: 'character-details/:id', loadChildren: () => import('./components/pages/characters/character-details/character-details.module').then(m => m.CharacterDetailsModule) 
+  },
+  { path: 'login', loadChildren: () => import('./components/pages/login/login.module').then(m => m.LoginModule) },
+  { path: 'signup', loadChildren: () => import('./components/pages/signup/signup.module').then(m => m.SignupModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/shared/interfaces/character.interface.ts
@@ -0,0 +1,8 @@
+export interface Character {
+    id: number;
+    name: string;
+    price: number;
+    category: string;
+    stock: number;
+    date: Date;
+}
